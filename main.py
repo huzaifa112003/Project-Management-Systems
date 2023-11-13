5bf60145dac5adcbb5140526d4880f4d97a8d607
Syed Maaz Ullah Shah <77611374+xomaaz@users.noreply.github.com>
2023-11-14 03:29:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ class MainWindow(QMainWindow):
 
         self.lineEdit_2.setEchoMode(QLineEdit.EchoMode.Password)
         self.pushButton.clicked.connect(self.login)
+        self.pushButton_2.clicked.connect(self.register)
 
     
     def login(self):
@@ -46,6 +47,116 @@ class MainWindow(QMainWindow):
             QMessageBox.information(self, 'Login Successful', 'Login Successful')
         else:
             QMessageBox.warning(self, 'Incorrect Email or Password', 'Incorrect Email or Password')
+
+
+    def register(self):
+        # Open the registration form
+        self.register = RegisterForm()
+        self.register.show()
+
+
+class RegisterForm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi('register.ui', self)
+
+        # Establish the database connection
+        self.connection = pyodbc.connect(
+                'DRIVER={ODBC Driver 17 for SQL Server};'
+                'SERVER=MAAZ-ULLAH\SQLEXPRESS;'
+                'DATABASE=Project;'
+                'Trusted_Connection=yes;'
+        )
+        self.cursor = self.connection.cursor()
+
+        self.pushButton.clicked.connect(self.register)
+        self.load_roles()
+        self.load_departments()
+
+    def load_roles(self):
+        try:
+            # Retrieve roles from the database
+            role_query = "SELECT RoleName FROM Role"
+            roles = self.cursor.execute(role_query).fetchall()
+
+            # Load roles into the combobox
+            self.comboBox_2.addItems([role[0] for role in roles])
+        except pyodbc.Error as e:
+            print(f"Database error: {e}")
+            QMessageBox.warning(self, 'Database Error', 'An error occurred while loading roles.')
+
+    def load_departments(self):
+        try:
+            # Retrieve departments from the database
+            department_query = "SELECT DepartmentName FROM Department"
+            departments = self.cursor.execute(department_query).fetchall()
+
+            # Load departments into the combobox
+            self.comboBox.addItems([department[0] for department in departments])
+        except pyodbc.Error as e:
+            print(f"Database error: {e}")
+            QMessageBox.warning(self, 'Database Error', 'An error occurred while loading departments.')
+
+    def register(self):
+        # Get the entered values from the registration form
+        name = self.lineEdit.text()
+        email = self.lineEdit_2.text()
+        password = self.lineEdit_3.text()
+        confirm_password = self.lineEdit_4.text()
+        job_title = self.lineEdit_7.text()
+
+        # Get selected role and department from the comboboxes
+        role_name = self.comboBox_2.currentText()
+        department_name = self.comboBox.currentText()
+
+        # Check if the password and confirm password match
+        if password != confirm_password:
+            QMessageBox.warning(self, 'Password Mismatch', 'Password and Confirm Password do not match.')
+            return
+
+        try:
+            # Check if the email already exists in the Users table
+            email_check_query = "SELECT COUNT(*) FROM Users WHERE Email = ?"
+            email_count = self.cursor.execute(email_check_query, (email,)).fetchone()[0]
+
+            if email_count > 0:
+                QMessageBox.warning(self, 'Email Already Exists', 'Email address is already registered.')
+                return
+
+            # Retrieve RoleID for the given RoleName
+            role_query = "SELECT RoleID FROM Role WHERE RoleName = ?"
+            role_result = self.cursor.execute(role_query, (role_name,)).fetchone()
+
+            if not role_result:
+                QMessageBox.warning(self, 'Role not found', f'Role "{role_name}" not found.')
+                return
+
+            role_id = role_result[0]
+
+            # Retrieve DepartmentID for the given DepartmentName
+            department_query = "SELECT DepartmentID FROM Department WHERE DepartmentName = ?"
+            department_result = self.cursor.execute(department_query, (department_name,)).fetchone()
+
+            if not department_result:
+                QMessageBox.warning(self, 'Department not found', f'Department "{department_name}" not found.')
+                return
+
+            department_id = department_result[0]
+
+            # Insert a new user into the Users table
+            insert_query = """
+                INSERT INTO Users (Name, Email, Password, RoleID, JobTitle, DepartmentID)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """
+            self.cursor.execute(insert_query, (name, email, password, role_id, job_title, department_id))
+            self.connection.commit()
+
+            QMessageBox.information(self, 'Registration Successful', 'User registered successfully.')
+        except pyodbc.Error as e:
+            print(f"Database error: {e}")
+            QMessageBox.warning(self, 'Database Error', 'An error occurred while registering the user.')
+
+            
 
 
 if __name__ == "__main__":
