0974ceb83cfecdb89bd289dccb944121b581c768
huzaifa112003 <mr07741@st.habib.edu.pk>
2023-12-01 19:08:08 +0500

    ew

--- populate.sql
+++ populate.sql
@@ -1,12 +1,3 @@
-INSERT INTO Department (DepartmentName)
-VALUES 
-    ('IT'),
-    ('Marketing'),
-    ('Finance'),
-    ('Human Resources'),
-    ('Research and Development'),
-    ('Customer Support');
-
 -- Inserting data into the Role table with additional roles
 INSERT INTO Role (RoleName)
 VALUES 
@@ -36,6 +27,14 @@ VALUES
     ('Research Scientist'),
     ('Technical Support Engineer');
 
+INSERT INTO Department (DepartmentName)
+VALUES 
+    ('IT'),
+    ('Marketing'),
+    ('Finance'),
+    ('Human Resources'),
+    ('Research and Development'),
+    ('Customer Support');
 
 INSERT INTO TaskStatus (StatusID, StatusName)
 VALUES 
@@ -54,3 +53,31 @@ VALUES
     ('Tech Solutions', 'Alice Williams', 'alice.williams@techsolutions.com', '888-777-6666'),
     ('Global Ventures', 'Charlie Brown', 'charlie.brown@globalventures.com', '444-333-2222');
 
+
+INSERT INTO Users (Name, Email, Password, RoleID, DepartmentID)
+VALUES 
+('John Doe', 'john.doe@example.com', 'password123', 1, 1),
+('Jane Smith', 'jane.smith@example.com', 'password123', 2, 2),
+('Alice Johnson', 'alice.johnson@example.com', 'password123', 3, 3),
+('Bob Brown', 'bob.brown@example.com', 'password123', 4, 4);
+
+
+INSERT INTO Project (ProjectName, Description, EndDate, ManagerID, ClientID)
+VALUES 
+('Website Redesign', 'Complete overhaul of the company website', '2024-06-30', 1, 1),
+('Social Media Campaign', 'Launching a new social media marketing campaign', '2023-12-31', 1, 2),
+('Product Launch', 'Introducing new product line', '2024-09-30', 1, 3);
+
+
+INSERT INTO Task (TaskName, Description, EndDate, StatusID, ProjectID, AssigneeID)
+VALUES 
+('Design Website Layout', 'Create the layout for the new website', '2023-06-15', 2, 1, 1),
+('Develop Marketing Content', 'Develop content for social media posts', '2023-05-20', 1, 2, 2),
+('Quality Assurance Testing', 'Test the new product for quality assurance', '2024-05-30', 3, 3, 3);
+
+
+INSERT INTO Report (ReportName, Description, Date, ProjectID)
+VALUES 
+('Website Design Review', 'Initial review of the website redesign project', '2023-01-15', 1),
+('Marketing Campaign Analysis', 'Analysis of the social media campaign performance', '2023-04-10', 2),
+('Product Testing Feedback', 'Feedback on the product testing phase', '2024-02-20', 3);
